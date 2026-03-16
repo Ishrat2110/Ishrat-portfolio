c24f33c107d5e10224d76beec0e64ea092f24887
Ishrat2110 <ishratjandu21@gmail.com>
2026-03-16 18:03:09 -0500

    update navbar, loading screen and title with Ishrat info

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -62,8 +62,8 @@ const Loading = ({ percent }: { percent: number }) => {
       <div className="loading-screen">
         <div className="loading-marquee">
           <Marquee>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
-            <span> Full Stack Developer</span> <span>Software Engineer</span>
+            <span> Agricultural Systems Engineer</span> <span>ML Researcher</span>
+            <span> Agricultural Systems Engineer</span> <span>ML Researcher</span>
           </Marquee>
         </div>
         <div
